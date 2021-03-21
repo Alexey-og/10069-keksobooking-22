0cc4205ecf982f057f5834a2596c7b0e50d3b410
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2021-03-21 15:01:47 +0300

    Доработка модуля загрузки превьюшек (avatar.js)

--- js/avatar.js
+++ js/avatar.js
@@ -1,72 +1,26 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const AVATAR_DEFAULT_URL = ['img/muffin-grey.svg'];
 
-const avatarChooser = document.querySelector('.ad-form-header__input');
+const image = {
+  WIDTH: '70',
+  HEIGHT: '70',
+}
+
+const avatarChooser = document.querySelector('.ad-form__field input[type=file]');
 const avatarPreview = document.querySelector('.ad-form-header__preview img');
 
 const photoChooser = document.querySelector('.ad-form__upload input[type=file]');
-const photoContainer = document.querySelector('.ad-form__photo-container');
 const photoPreview = document.querySelector('.ad-form__photo');
 
-/* avatarChooser.addEventListener('change', () => {
-  const file = avatarChooser.files[0];
-  const fileName = file.name.toLowerCase();
-
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
-
-  if (matches) {
-    const reader = new FileReader();
-
-    reader.readAsDataURL(file);
-
-    reader.addEventListener('load', () => {
-      avatarPreview.src = reader.result;
-    });
-  }
-}); */
-
-const createImage = (imageSrc) => {
-  const image = {
-    WIDTH: '70',
-    HEIGHT: '70',
-    ALT: 'Фотография жилья',
-  }
+const createImage = () => {
   const imageElement = document.createElement('IMG');
-  imageElement.src = imageSrc;
-  imageElement.alt = image.ALT;
   imageElement.width = image.WIDTH;
   imageElement.height = image.HEIGHT;
   return imageElement;
 }
 
 const uploadImage = (imageChooser, image) => {
-  imageChooser.addEventListener('change', () => {
-    const file = imageChooser.files[0];
-    const fileName = file.name.toLowerCase();
-
-    const matches = FILE_TYPES.some((it) => {
-      return fileName.endsWith(it);
-    });
-
-    if (matches) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.addEventListener('load', () => {
-        image.src = reader.result;
-      });
-    }
-  });
-}
-
-const uploadAvatar = (chooser, image) => {
-  uploadImage(chooser, image);
-}
-
-
-photoChooser.addEventListener('change', () => {
-  const photoElement = photoPreview.cloneNode();
-  const file = photoChooser.files[0];
+  const file = imageChooser.files[0];
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => {
@@ -75,13 +29,29 @@ photoChooser.addEventListener('change', () => {
 
   if (matches) {
     const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      photoElement.append(createImage(reader.result));
-      photoContainer.append(photoElement);
-    });
     reader.readAsDataURL(file);
+    reader.addEventListener('load', () => {
+      image.src = reader.result;
+    });
   }
+}
+
+avatarChooser.addEventListener('change', () => {
+  uploadImage(avatarChooser, avatarPreview);
 });
 
+photoChooser.addEventListener('change', () => {
+  const image = createImage();
+  uploadImage(photoChooser, image);
+  photoPreview.append(image);
+});
 
-uploadAvatar(avatarChooser, avatarPreview);
+const clearAvatar = () => {
+  avatarPreview.src = AVATAR_DEFAULT_URL;
+  photoPreview.textContent = '';
+};
+
+
+export {
+  clearAvatar
+};
